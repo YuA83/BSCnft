920e338e254b479e8141f0fa7994258662560932
KyeongA Yu <kayu.citylabs@gmail.com>
2021-12-16 21:41:44 +0900

    update2

---
+++ solidity/contracts/BscNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BscNFT is ERC721URIStorage {
+    
+}
--- solidity/migrations/2_deploy_contract.js
+++
@@ -1,9 +0,0 @@
-const BFT = artifacts.require("BFT");
-const Market = artifacts.require("Market");
-
-module.exports = async (deployer) => {
-    await deployer.deploy(BFT);
-
-    const deployedBFT = BFT.deployed();
-    await deployer.deploy(Market, deployedBFT.address);
-};
